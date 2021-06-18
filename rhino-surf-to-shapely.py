5ff5e01879d8a54afc8c85b8b6178b1119b79b20
NormanMorman <normanrichardson@gmail.com>
2021-06-18 06:28:07 -0700

    Added RHCurv curve
    * Class for a rhino curve
    * The class knows if it is linear (so it does not need to be refinded)
    * Each curve can be refined by adding additional points between the greville points.

--- rhino-surf-to-shapely.py
+++ rhino-surf-to-shapely.py
@@ -1,58 +1,51 @@
 import rhino3dm as rh
-from shapely.geometry import MultiLineString
+from shapely.geometry import MultiLineString, LineString
 from shapely.ops import polygonize
 import matplotlib.pyplot as plt
 import numpy as np
 import json
-from geomdl import utilities, NURBS, multi
-from geomdl.visualization import VisMPL
 
-lines = []
+class RHCurv:
+    def __init__(self, curv):
+        self._curv = curv
+        self._nurb = curv.ToNurbsCurve()
+        self._greville_points_param = [self._nurb.GrevilleParameter(idx) for idx in range(len(self._nurb.Points))]
+        self._degree = self._nurb.Order-1
+        self._greville_points_param_modif = self._greville_points_param
+    def refine(self,num):
+        gen_interv = ((self._greville_points_param[ii], self._greville_points_param[ii+1]) for ii in range(len(self._greville_points_param)-1))
+        self._greville_points_param_modif = [self._greville_points_param[0]]
+        for ii, jj in gen_interv:
+            self._greville_points_param_modif += list(np.linspace(ii,jj,num+2)[1:])
+    def get_shapely_line(self):
+        line = []
+        for t in self._greville_points_param_modif:
+            pnt = self._curv.PointAt(t)
+            line.append((pnt.X, pnt.Y, pnt.Z))
+        return LineString(line)
+    def is_line(self):
+        return self._curv.IsLinear()
+
+def convert_and_refine(rh_geom, refine_num):
+    rh_curvs = []
+    for ii in range(0,len(rh_geom.Edges)):
+        rh_curvs.append(RHCurv(brep.Edges[ii]))
+    for rh_curv in rh_curvs:
+        if not rh_curv.is_line(): 
+            rh_curv.refine(refine_num)
+    ml = MultiLineString([rc.get_shapely_line() for rc in rh_curvs])
+    pgs = list(polygonize(ml))
+    return pgs[0], pgs[1:]
+
+refine = 1
 
 with open('data',"r") as f:
     json_data = f.read()
 brep_dict = json.loads(json_data)
 brep = rh.CommonObject.Decode(brep_dict[2])
 
-for ii in range(0,len(brep.Edges)):
-    if brep.Edges[ii].IsLinear():
-        s_p = brep.Edges[ii].PointAtStart
-        e_p = brep.Edges[ii].PointAtEnd
-        l_points = [(s_p.X, s_p.Y, s_p.Z), (e_p.X, e_p.Y, e_p.Z)]
-        lines.append(l_points)
-    else: 
-        nurb = brep.Edges[ii].ToNurbsCurve()
-        l_points = []
-        for idx, point in enumerate(nurb.Points):
-            if point.W==1: l_points.append((point.X, point.Y, point.Z))
-        lines.append(l_points)
+ploy1, holes1 = convert_and_refine(brep, refine)
 
-rh.NurbsCurve.MakePiecewiseBezier
-
-ml = MultiLineString(lines)
-
-pgs = (list(polygonize(ml)))
-
-[plt.plot(*pgs[0].exterior.xy)]
-[plt.plot(*ii.xy) for ii in pgs[0].interiors]
+[plt.plot(*ploy1.exterior.xy)]
+[plt.plot(*ii.xy) for ii in ploy1.interiors]
 plt.show()
-
-
-mcrv = multi.CurveContainer()
-for ii in range(0,len(brep.Edges)):
-    nurb = brep.Edges[ii].ToNurbsCurve()
-    print(f"ii: {ii}")
-    print(f"deg: {nurb.Order-1}")
-    print(f"num cp: {len(nurb.Points)}")
-    print(f"num knots: {len(nurb.Knots)+2}")
-    curve = NURBS.Curve()
-    curve.order = nurb.Order
-    curve.ctrlpts = [(point.X, point.Y, point.Z) for point in nurb.Points]
-    curve.weights = [point.W for point in nurb.Points]
-    knots_all = ([nurb.Knots.SuperfluousKnot(True)]+[knot for knot in nurb.Knots]+[nurb.Knots.SuperfluousKnot(False)])
-    curve.knotvector = knots_all
-    curve.delta = 0.01
-    mcrv.add(curve)
-vis_config = VisMPL.VisConfig(legend=False, axes=True, figure_dpi=120)
-mcrv.vis = VisMPL.VisCurve2D(vis_config)
-mcrv.render()
