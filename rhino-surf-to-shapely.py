ca42d508befbefc00602cc0d77c4f9ae5cabb3d1
NormanMorman <normanrichardson@gmail.com>
2021-06-18 03:27:03 -0700

    Exploring with geomdl nurbs

--- rhino-surf-to-shapely.py
+++ rhino-surf-to-shapely.py
@@ -4,12 +4,16 @@ from shapely.ops import polygonize
 import matplotlib.pyplot as plt
 import numpy as np
 import json
-
-with open('data',"r") as f:
-    brep_dict = json.load(f)
+from geomdl import utilities, NURBS, multi
+from geomdl.visualization import VisMPL
 
 lines = []
-brep = rh.CommonObject.Decode(brep_dict)
+
+with open('data',"r") as f:
+    json_data = f.read()
+brep_dict = json.loads(json_data)
+brep = rh.CommonObject.Decode(brep_dict[2])
+
 for ii in range(0,len(brep.Edges)):
     if brep.Edges[ii].IsLinear():
         s_p = brep.Edges[ii].PointAtStart
@@ -32,3 +36,23 @@ pgs = (list(polygonize(ml)))
 [plt.plot(*pgs[0].exterior.xy)]
 [plt.plot(*ii.xy) for ii in pgs[0].interiors]
 plt.show()
+
+
+mcrv = multi.CurveContainer()
+for ii in range(0,len(brep.Edges)):
+    nurb = brep.Edges[ii].ToNurbsCurve()
+    print(f"ii: {ii}")
+    print(f"deg: {nurb.Order-1}")
+    print(f"num cp: {len(nurb.Points)}")
+    print(f"num knots: {len(nurb.Knots)+2}")
+    curve = NURBS.Curve()
+    curve.order = nurb.Order
+    curve.ctrlpts = [(point.X, point.Y, point.Z) for point in nurb.Points]
+    curve.weights = [point.W for point in nurb.Points]
+    knots_all = ([nurb.Knots.SuperfluousKnot(True)]+[knot for knot in nurb.Knots]+[nurb.Knots.SuperfluousKnot(False)])
+    curve.knotvector = knots_all
+    curve.delta = 0.01
+    mcrv.add(curve)
+vis_config = VisMPL.VisConfig(legend=False, axes=True, figure_dpi=120)
+mcrv.vis = VisMPL.VisCurve2D(vis_config)
+mcrv.render()
