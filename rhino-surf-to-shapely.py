3972e9377244727c94b13359c3d01fdbb7bf1af4
NormanMorman <normanrichardson@gmail.com>
2021-06-21 13:55:59 -0700

    Add class for rhino file

--- rhino-surf-to-shapely.py
+++ rhino-surf-to-shapely.py
@@ -5,7 +5,48 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 
-class RHCurv:
+# rh.Surface.IsPlanar()
+
+class RhFile:
+    def __init__(self, file_name):
+        self.validate(file_name)
+        self._file_name = file_name
+        model = rh.File3dm.Read(file_name)
+        self._objects = model.Objects
+        #print(self._objects[1].Geometry)
+        self.process_objects()
+    @staticmethod
+    def validate(file_name):
+        #does the file exist
+        pass
+    @staticmethod
+    def validate_geom(self, geom):
+        valid = False
+        if isinstance(geom, rh.Surface):
+            valid = geom.IsPlanar()
+        # if isinstance(geom, rh.Curve):
+        #     valid = geom.IsPlanar()
+        #     valid = geom.IsClosed
+        return valid
+        # Is surface planer, ok get boundary
+        # not done yet... What transformation gets it onto the x,y plane.
+        # test plane
+    def process_surface(self):
+        pass
+    def process_objects(self):
+        self._surfaces = []
+        # curves = {}
+        for obj in self._objects:
+            # geom = obj.Geometry
+            # if isinstance(geom, rh.Curve):
+            #     frame = geom.FrameAt(0)
+            #     p_normal = frame[1].ZAxis
+            #     if p_normal in curves: curves[p_normal].append(geom)
+            #     else: curves[p_normal] = [geom]
+            # else:
+            self._surfaces.append(obj.Geometry)
+
+class RhCurv:
     def __init__(self, curv):
         self._curv = curv
         self._nurb = curv.ToNurbsCurve()
@@ -29,7 +70,7 @@ class RHCurv:
 def convert_and_refine(rh_geom, refine_num):
     rh_curvs = []
     for ii in range(0,len(rh_geom.Edges)):
-        rh_curvs.append(RHCurv(brep.Edges[ii]))
+        rh_curvs.append(RhCurv(rh_geom.Edges[ii]))
     for rh_curv in rh_curvs:
         if not rh_curv.is_line(): 
             rh_curv.refine(refine_num)
@@ -37,7 +78,7 @@ def convert_and_refine(rh_geom, refine_num):
     pgs = list(polygonize(ml))
     return pgs[0], pgs[1:]
 
-refine = 1
+refine = 0
 
 with open('data',"r") as f:
     json_data = f.read()
@@ -49,3 +90,5 @@ ploy1, holes1 = convert_and_refine(brep, refine)
 [plt.plot(*ploy1.exterior.xy)]
 [plt.plot(*ii.xy) for ii in ploy1.interiors]
 plt.show()
+
+RhFile('AAA.3dm')
