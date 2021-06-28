310d343a26364eaa66554d36a8f0231c31db7e01
NormanMorman <normanrichardson@gmail.com>
2021-06-28 06:01:06 -0700

    Add documentation to RhCurv

--- rhino_to_shapely.py
+++ rhino_to_shapely.py
@@ -23,7 +23,7 @@ class CoordTransform:
 
     Methods
     -------
-    transform_rh_sh(pnts) :
+    transform(pnts) :
         Transforms a coordinate from the (x,y,z) into (x',y')
     """
     def __init__(self, vec1, vec2):
@@ -327,23 +327,72 @@ class RhImporter:
         pass
 
 class RhCurv:
+    """Wrapper for a rhino curve.
+
+    Parameters
+    ----------
+    curv :
+        A rhino.Curve
+
+    Methods
+    -------
+    refine(num) :
+        Refine the individual Bézier curves of the rhino.Curve
+    get_shapely_line(transform) :
+        Get the shapely line string for the rhino curve.
+    is_line() :
+        Is the rhino line a straight line
+    """
     def __init__(self, curv):
+        """Constructor
+
+        Parameters
+        ----------
+        curv : rhino3dm.Curve
+            A rhino3dm curve
+        """
         self._curv = curv
         self._nurb = curv.ToNurbsCurve()
         self._greville_points_param = [self._nurb.GrevilleParameter(idx) for idx in range(len(self._nurb.Points))]
         self._degree = self._nurb.Order-1
         self._greville_points_param_modif = self._greville_points_param
     def refine(self,num):
+        """Refine the individual Bézier curves of the rhino.Curve
+
+        Parameters
+        ----------
+        num : integer
+            Number of refinements
+        """
         gen_interv = ((self._greville_points_param[ii], self._greville_points_param[ii+1]) for ii in range(len(self._greville_points_param)-1))
         self._greville_points_param_modif = [self._greville_points_param[0]]
         for ii, jj in gen_interv:
             self._greville_points_param_modif += list(np.linspace(ii,jj,num+2)[1:])
     def get_shapely_line(self, transform):
+        """Get the shapely line string for the rhino curve.
+
+        Parameters
+        ----------
+        transform : func
+            A function that transforms (3,n) ndarray into a new coordinate system.
+
+        Returns
+        -------
+        Shapely.Geometry.LineString
+            The discretized shapely representation of a rhino curve.
+        """
         pnts = []
         for t in self._greville_points_param_modif:
             pnt = self._curv.PointAt(t)
             pnts.append([pnt.X, pnt.Y, pnt.Z])
         pnts_np = transform(np.array(pnts).T).round(decimals=12)
         return asLineString(pnts_np.T)
+    
     def is_line(self):
+        """Is the rhino line a straight line
+
+        Returns
+        -------
+        Boolean
+        """
         return self._curv.IsLinear()
