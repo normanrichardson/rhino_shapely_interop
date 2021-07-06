9712afb7a29113f7b2353339701ffb0839d2d4bc
NormanMorman <normanrichardson@gmail.com>
2021-07-06 11:38:30 -0700

    Include unit tests for RhImporter

--- unittests.py
+++ unittests.py
@@ -1,9 +1,10 @@
-from shapely.geometry.multipoint import MultiPoint
-from rhino_to_shapely import CoordTransform, RhCurv, RhPnt
+from numpy.core.numeric import isclose
+from rhino_to_shapely import CoordTransform, RhCurv, RhPnt, RhImporter
 import unittest
-from shapely.geometry import Point, asMultiPoint
+from shapely.geometry import Point, asMultiPoint, Polygon, LineString, MultiPoint
 import numpy as np
 import rhino3dm as rh
+import matplotlib.pyplot as plt
 
 class TestCoordTransform(unittest.TestCase):
     
@@ -86,6 +87,110 @@ class TestRhPnt(unittest.TestCase):
         rp = RhPnt(pnt)
         test = rp.get_shapely_point(transform)
         np.testing.assert_array_almost_equal(test, exp)
+
+class TestRhinoImporterValidation(unittest.TestCase):
+    def test_validate_file_name(self):
+        self.assertTrue(RhImporter._validate_file_name("unit_circle.3dm"))
+
+    def test_validate_brep(self):
+        pln = rh.Plane(rh.Point3d(0,0,0), rh.Vector3d(0,0,1))
+        bound = rh.Polyline(5)
+        bound.Add(1,1,0)
+        bound.Add(-1,1,0)
+        bound.Add(-1,-1,0)
+        bound.Add(1,-1,0)
+        bound.Add(1,1,0)
+        brep = rh.Brep.CreateTrimmedPlane(pln, bound.ToPolylineCurve())
+        self.assertTrue(RhImporter._validate_brep(brep))
+
+    def test_validate_curve(self):
+        pl = rh.Polyline(3)
+        pl.Add(1,1,4)
+        pl.Add(2,2,4)
+        pl.Add(3,1,4)
+        plc = pl.ToPolylineCurve()
+        self.assertTrue(RhImporter._validate_curve(plc))
+
+    def test_validate_point(self):
+        pnt1 = rh.Point3d(1,2,4)
+        pnt2 = rh.Point2d(1,2)
+        self.assertTrue(RhImporter._validate_point(pnt1))
+        self.assertTrue(RhImporter._validate_point(pnt2))
+
+class TestRhinoImporterCreation(unittest.TestCase):
+    def test_creation_from_file(self):
+        rhi = RhImporter.from_file("unit_circle.3dm")
+        self.assertEqual(len(rhi._point), 0)
+        self.assertEqual(len(rhi._curve), 1)
+        self.assertEqual(len(rhi._brep), 0)
+
+    def test_creation_from_file_byte_array(self):
+        pass
+
+    def test_creation_from_serialzed_brep(self):
+        pln = rh.Plane(rh.Point3d(0,0,0), rh.Vector3d(0,0,1))
+        bound = rh.Polyline(4)
+        bound.Add(0,1,0)
+        bound.Add(-1,-1,0)
+        bound.Add(1,-1,0)
+        bound.Add(0,1,0)
+        brep = rh.Brep.CreateTrimmedPlane(pln, bound.ToPolylineCurve())
+        rhi = RhImporter.from_serialzed_brep(brep.Encode())
+        self.assertEqual(len(rhi._point), 0)
+        self.assertEqual(len(rhi._curve), 0)
+        self.assertEqual(len(rhi._brep), 1)
+
+    def test_creation_from_serialzed_curve(self):
+        pl = rh.Polyline(3)
+        pl.Add(1,1,4)
+        pl.Add(2,2,4)
+        pl.Add(3,1,4)
+        plc = pl.ToPolylineCurve()
+        rhi = RhImporter.from_serialzed_curve(plc.Encode())
+        self.assertEqual(len(rhi._point), 0)
+        self.assertEqual(len(rhi._curve), 1)
+        self.assertEqual(len(rhi._brep), 0)
+
+class TestRhinoImporterExports(unittest.TestCase):
+    def test_get_planer_brep(self):
+        pln = rh.Plane(rh.Point3d(0,0,0), rh.Vector3d(0,0,1))
+        bound = rh.Polyline(4)
+        bound.Add(0,1,0)
+        bound.Add(-1,-1,0)
+        bound.Add(1,-1,0)
+        bound.Add(0,1,0)
+        brep = rh.Brep.CreateTrimmedPlane(pln, bound.ToPolylineCurve())
+        rhi = RhImporter.from_serialzed_brep(brep.Encode())
+        test = [poly for poly in rhi.get_planer_brep(0)][0]
+        exp = Polygon([(0,1), (-1,-1), (1,-1)])
+        self.assertTrue((test-exp).is_empty)
+
+    def test_get_curves(self):
+        pl = rh.Polyline(5)
+        pl.Add(1,1,0)
+        pl.Add(-1,1,0)
+        pl.Add(-1,-1,0)
+        pl.Add(1,-1,0)
+        pl.Add(1,1,0)
+        crv = pl.ToPolylineCurve()
+        rhi = RhImporter.from_serialzed_curve(crv.Encode())
+        test = [crv for crv in rhi.get_curves(0)][0]
+        exp = LineString([(1,1), (-1,1), (-1,-1), (1,-1), (1,1)])
+        self.assertTrue((test-exp).is_empty)
+
+    def test_get_points(self):
+        pnt1 = rh.Point3d(5,2,3)
+        pnt2 = rh.Point3d(8,2,2)
+        model = rh.File3dm()
+        model.Objects.AddPoint(pnt1)
+        model.Objects.AddPoint(pnt2)
+        rhi = RhImporter(model=model)
+        test = asMultiPoint([poly for poly in rhi.get_points()])
+        exp = MultiPoint([(5,2), (8,2)])
+        self.assertTrue((test-exp).is_empty)
+        test = asMultiPoint([poly for poly in rhi.get_points(np.array([0,0,1]),np.array([0,1,0]))])
+        exp = MultiPoint([(3,2), (2,2)])
+        self.assertTrue((test-exp).is_empty)
 
 if __name__=='__main__':
       unittest.main()
